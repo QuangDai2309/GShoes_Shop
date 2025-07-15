714ca84b9bd67045e55fe47cb33bdbb6593edc27
QuangDai2309 <daitdqps38672@gmail.com>
2025-07-15 16:04:18 +0700

    Cài Mailer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1f974e8a84a6a29be78ab9331664a84f9a36222e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1f974e8a84a6a29be78ab9331664a84f9a36222e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
